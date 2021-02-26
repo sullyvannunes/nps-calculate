78aa0c6ffaa0f92d971f3bf22c2c91f258fdd098
Sullyvan Nunes <sully@MacBook-Pro-de-Sullyvan.local>
2021-02-26 13:52:04 -0300

    add db connection

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UsersController {
 
   async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+    const userRepository = getRepository(User);
+
+    const user = userRepository.create({ name, email });
     
+    return response.json(await userRepository.save(user));
   }
 }
 
